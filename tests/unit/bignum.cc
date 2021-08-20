7c1b3d302ec87392b906515d7309f56d63e81ff8
Bruno BELANYI <bruno@belanyi.fr>
2021-08-20 17:22:21 +0200

    tests: unit: add bignum tests

---
+++ tests/unit/bignum.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include "bignum.hh"
+
+using namespace abacus::bignum;
+
+TEST(BigNum, equality) {
+    auto const zero = BigNum(0);
+    auto const one = BigNum(1);
+
+    ASSERT_EQ(zero, zero);
+    ASSERT_EQ(one, one);
+    ASSERT_NE(zero, one);
+}
+
+TEST(BigNum, comparisons) {
+    auto const zero = BigNum(0);
+    auto const one = BigNum(1);
+
+    ASSERT_LT(zero, one);
+    ASSERT_LE(zero, one);
+    ASSERT_LE(zero, zero);
+
+    ASSERT_GT(one, zero);
+    ASSERT_GE(one, zero);
+    ASSERT_GE(one, one);
+}
